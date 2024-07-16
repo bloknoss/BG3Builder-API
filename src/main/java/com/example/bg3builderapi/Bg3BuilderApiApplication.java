88ecfa50d422c10453eba06500b1d0b7bf8525db
bloknoss <windpourwow@gmail.com>
2024-07-16 21:07:44 +0200

    Initial Commit

---
+++ src/main/java/com/example/bg3builderapi/Bg3BuilderApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.bg3builderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bg3BuilderApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bg3BuilderApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bg3builderapi/Bg3BuilderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bg3builderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bg3BuilderApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
